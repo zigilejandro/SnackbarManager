76cf4137f5be7c6bab7d53b5c7d8e59ed82f879f
Alejandro Reyna <ealvarez@eaglehubs.com>
2024-04-24 14:16:29 -0600

    main: Added basic navigator

--- android/app/src/main/java/com/snackbarmanager/MainActivity.kt
+++ android/app/src/main/java/com/snackbarmanager/MainActivity.kt
@@ -5,8 +5,17 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import android.os.Bundle
+
 class MainActivity : ReactActivity() {
 
+  /**
+    React navigation config
+  */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -19,4 +28,5 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
 }
