b139042658a5a46b6d63dd748e189e37f55816af
Alejandro Reyna <ealvarez@eaglehubs.com>
2024-04-24 16:18:29 -0600

    main: Added snackbar manager on App

--- App.tsx
+++ App.tsx
@@ -1,26 +1,45 @@
 // In App.js in a new project
 
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PaperProvider, Button } from 'react-native-paper';
+import { SnackbarManager, useSnackbarManager } from './SnackbarManager';
 
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
+  const { addSnackbar } = useSnackbarManager()
+
+  const redirect = () => {
+    addSnackbar(`youre now on details screen`)
+    navigation.navigate('Details')
+  } 
   return (
+    <>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+        onPress={redirect}
+      >Go to Details</Button>
     </View>
+    </>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
+  const { addSnackbar } = useSnackbarManager()
+
+  const redirect = () => {
+    addSnackbar(`youre now on home screen`)
+    navigation.navigate('Home')
+  } 
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        onPress={redirect}
+      >Go Home</Button>
     </View>
   );
 }
@@ -28,12 +47,16 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+        <NavigationContainer>
+          <SnackbarManager>
+              <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+              </Stack.Navigator>
+          </SnackbarManager>
+        </NavigationContainer>
+    </PaperProvider>
   );
 }
 
